5ca4cbc0d4c7b90dd08d6d3bbe997ba87d9abe5b
franneck94 <schaffranek.jan@web.de>
2022-02-07 13:26:37 +0100

    up

--- 10_Advanced/AdasGui/src/Render.cc
+++ 10_Advanced/AdasGui/src/Render.cc
@@ -1,9 +1,37 @@
+#include <algorithm>
+#include <array>
+#include <vector>
+
+#include "imgui.h"
+#include "implot.h"
+
+#include "DataLoader.hpp"
+#include "DataLoaderConstants.hpp"
 #include "Render.hpp"
+#include "RenderConstants.hpp"
 
 void render_cycle(const VehicleType &ego_vehicle, const NeighborVehiclesType &vehicles)
 {
+    ImGui::SetNextWindowPos(ImVec2(0.0F, 0.0F));
+    ImGui::SetNextWindowSize(ImVec2(WINDOWS_WIDTH, LANE_PLOT_TOTAL_HEIGHT));
+    if (ImGui::Begin("MainWindow", nullptr, WINDOW_FLAGS_CLEAN))
+    {
+        ImPlot::CreateContext();
+        plot_lanes(ego_vehicle, vehicles);
+        plot_table(ego_vehicle, vehicles);
+        ImGui::End();
+    }
+}
+
+
+void plot_lanes(const VehicleType &ego_vehicle, const NeighborVehiclesType &vehicles)
+{
     (void)ego_vehicle;
     (void)vehicles;
+}
 
-    return;
+void plot_table(const VehicleType &ego_vehicle, const NeighborVehiclesType &vehicles)
+{
+    (void)ego_vehicle;
+    (void)vehicles;
 }
--- 10_Advanced/AdasGui/src/Render.hpp
+++ 10_Advanced/AdasGui/src/Render.hpp
@@ -3,3 +3,7 @@
 #include "DataLoader.hpp"
 
 void render_cycle(const VehicleType &ego_vehicle, const NeighborVehiclesType &vehicles);
+
+void plot_lanes(const VehicleType &ego_vehicle, const NeighborVehiclesType &vehicles);
+
+void plot_table(const VehicleType &ego_vehicle, const NeighborVehiclesType &vehicles);
