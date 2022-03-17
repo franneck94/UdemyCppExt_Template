90f7aaa5a57e8a5c8a1f1e9a8aa90cb2808973b1
franneck94 <schaffranek.jan@web.de>
2022-03-17 08:41:30 +0100

    up

--- 10_Lambda/UnitCalculations/Main.cc
+++ 10_Lambda/UnitCalculations/Main.cc
@@ -6,6 +6,46 @@ constexpr std::uint32_t TIME_SPAN = 50;
 
 void compute_velocities(const std::array<float, NUM_ENTRIES> long_velocities_mps,
                         const std::array<float, NUM_ENTRIES> lat_velocities_mps,
+                        std::array<float, NUM_ENTRIES> &velocities_mps);
+
+void compute_heading_degrees(const std::array<float, NUM_ENTRIES> long_velocities_mps,
+                             const std::array<float, NUM_ENTRIES> lat_velocities_mps,
+                             std::array<float, NUM_ENTRIES> &heading_degrees);
+
+void compute_accelerations(const std::array<float, NUM_ENTRIES> &velocities_mps,
+                           std::array<float, NUM_ENTRIES> &accelerations_mps2);
+
+void compute_rel_velocities(const std::array<float, NUM_ENTRIES> &velocities_mps,
+                            const std::array<float, NUM_ENTRIES> &ego_velocities_mps,
+                            std::array<float, NUM_ENTRIES> &rel_velocities_mps);
+
+void compute_rel_accelerations(const std::array<float, NUM_ENTRIES> &accelerations_mps2,
+                               const std::array<float, NUM_ENTRIES> &ego_accelerations_mps2,
+                               std::array<float, NUM_ENTRIES> &rel_accelerations_mps2);
+
+int main(int argc, char **argv)
+{
+    const auto ego_velocities_mps = std::array<float, NUM_ENTRIES>{25.0F, 25.6F, 26.2F};
+    const auto ego_accelerations_mps2 = std::array<float, NUM_ENTRIES>{0.0F, 0.6F, 0.6F};
+
+    const auto long_velocities_mps = std::array<float, NUM_ENTRIES>{23.0F, 25.0F, 27.0F};
+    const auto lat_velocities_mps = std::array<float, NUM_ENTRIES>{1.0F, 1.2F, 1.1F};
+
+    auto velocities_mps = std::array<float, NUM_ENTRIES>{};
+    auto heading_degrees = std::array<float, NUM_ENTRIES>{};
+    auto accelerations_mps2 = std::array<float, NUM_ENTRIES>{};
+    auto rel_velocities_mps = std::array<float, NUM_ENTRIES>{};
+    auto rel_accelerations_mps2 = std::array<float, NUM_ENTRIES>{};
+
+    compute_velocities(long_velocities_mps, lat_velocities_mps, velocities_mps);
+    compute_heading_degrees(long_velocities_mps, lat_velocities_mps, heading_degrees);
+    compute_accelerations(velocities_mps, accelerations_mps2);
+    compute_rel_velocities(long_velocities_mps, ego_velocities_mps, rel_velocities_mps);
+    compute_rel_accelerations(accelerations_mps2, ego_accelerations_mps2, rel_accelerations_mps2);
+}
+
+void compute_velocities(const std::array<float, NUM_ENTRIES> long_velocities_mps,
+                        const std::array<float, NUM_ENTRIES> lat_velocities_mps,
                         std::array<float, NUM_ENTRIES> &velocities_mps)
 {
     (void)long_velocities_mps;
@@ -45,25 +85,4 @@ void compute_rel_accelerations(const std::array<float, NUM_ENTRIES> &acceleratio
     (void)accelerations_mps2;
     (void)ego_accelerations_mps2;
     (void)rel_accelerations_mps2;
-}
-
-int main(int argc, char **argv)
-{
-    const auto ego_velocities_mps = std::array<float, NUM_ENTRIES>{25.0F, 25.6F, 26.2F};
-    const auto ego_accelerations_mps2 = std::array<float, NUM_ENTRIES>{0.0F, 0.6F, 0.6F};
-
-    const auto long_velocities_mps = std::array<float, NUM_ENTRIES>{23.0F, 25.0F, 27.0F};
-    const auto lat_velocities_mps = std::array<float, NUM_ENTRIES>{1.0F, 1.2F, 1.1F};
-
-    auto velocities_mps = std::array<float, NUM_ENTRIES>{};
-    auto heading_degrees = std::array<float, NUM_ENTRIES>{};
-    auto accelerations_mps2 = std::array<float, NUM_ENTRIES>{};
-    auto rel_velocities_mps = std::array<float, NUM_ENTRIES>{};
-    auto rel_accelerations_mps2 = std::array<float, NUM_ENTRIES>{};
-
-    compute_velocities(long_velocities_mps, lat_velocities_mps, velocities_mps);
-    compute_heading_degrees(long_velocities_mps, lat_velocities_mps, heading_degrees);
-    compute_accelerations(velocities_mps, accelerations_mps2);
-    compute_rel_velocities(long_velocities_mps, ego_velocities_mps, rel_velocities_mps);
-    compute_rel_accelerations(accelerations_mps2, ego_accelerations_mps2, rel_accelerations_mps2);
 }
